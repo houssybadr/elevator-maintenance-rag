dd29d2d56d1e3d3c09b0aed8fa25799a5cb6fc90
Houssy badr <63375609+houssybadr@users.noreply.github.com>
2026-08-20 15:50:48 +0100

    Update main.py

--- rag/app/main.py
+++ rag/app/main.py
@@ -57,10 +57,11 @@ app.add_exception_handler(RateLimitExceeded,_rate_limit_exceeded_handler)
 
 # Adding middlewares
 app.add_middleware(
-	CORSMiddleware,
-	allow_origins=["*"],
-	allow_methods=["POST","GET"],
-	allow_headers=["X-API-Key","Content-Type"]
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["POST", "GET"],
+    allow_headers=["X-API-Key", "Content-Type"],
 )
 
 # Adding routers
